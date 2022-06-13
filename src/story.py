bafe2847c659bd6d55af0a118692b0bf9320417d
fedenanni <nanni.federico@gmail.com>
2022-06-13 15:52:30 +0100

    Add get usage of bike

--- src/story.py
+++ src/story.py
@@ -1,9 +1,10 @@
 import pandas as pd 
-import bike 
+import bike, clean_data
 
-df = pd.read_csv('../test/data/9. Journey Data Extract 19Aug-20 Aug12.csv',sep=',')
+data = clean_data.load_clean_data('../test/data')
 
 bike = bike.Bike(id=8710)
-bike_story = bike.get_story(df)
+bike_story = bike.get_story(data)
+bike_usage = bike.get_usage(bike_story)
 
-print (bike_story.head())
+print (bike_usage)
