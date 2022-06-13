c1f7ef601bbf565767e013b1033c97da48dce5b7
crangelsmith <crangelsmith@turing.ac.uk>
2022-06-13 17:13:42 +0100

    end of day

--- src/trip.py
+++ src/trip.py
@@ -1,11 +1,31 @@
+import requests as requests
+
 class Trip:
 
-    def __init__(self, data, trip_id):
+    def __init__(self, data, trip_id, station_data):
         df = data[data.index == trip_id]
 
-        self.init_station = {'name': df.start_station_name.values[0], 'id': df.start_station_id.values[0]}
-        self.end_station = {'name': df.end_station_name.values[0], 'id': df.end_station_id.values[0]}
+        self.init_station = {'name': df.start_station_name.values[0], 'id': df.start_station_id.values[0],
+                             'latitude': station_data[df.start_station_name.values[0]]['lat'],
+                             'longitude':station_data[df.start_station_name.values[0]]['lon']}
+        self.end_station = {'name': df.end_station_name.values[0], 'id': df.end_station_id.values[0],
+                            'latitude': station_data[df.end_station_name.values[0]]['lat'],
+                            'longitude': station_data[df.end_station_name.values[0]]['lon']}
         self.bike = df.bike_id.values[0]
         self.duration = df.duration.values[0]
-        self.date = {'start':df.start_date.values[0], 'end':df.end_date.values[0]}
+        self.date = {'start': df.start_date.values[0], 'end': df.end_date.values[0]}
+
+
+
+
+    # def get_route(self):
+    #     registeredapikey = 'f339aa90aba2b309'
+    #     trip = 'https: //www.cyclestreets.net/api/journey.json?key='+registeredapikey+'&reporterrors=1&itinerarypoints =0.11795,52.20530'
+    #     response = requests.get(name)
+    #
+    #     name = 'https://www.cyclestreets.net/api/journey.json?key=f339aa90aba2b309&reporterrors=1&start='+self.init_station['name']+'&end='+self.end_station['name']+'&plan=quietest'
+    #     #print (response)
+    #
+
+
 
--- test/test_trip.py
+++ test/test_trip.py
@@ -1,11 +1,24 @@
+import json
+
 from src.clean_data import load_clean_data
 from src.trip import Trip
+from unittest import TestCase
+import json
 
-class TestTrip:
+class TestTrip(TestCase):
+
+    def setUp(self):
+        self.data =  load_clean_data('data/')
+        with open('data/stations_loc.json') as f:
+            self.stations = json.load(f)
+
+        self.trip = Trip(self.data, 15305469,self.stations)
+
     def test_init(self):
-        data = load_clean_data('data/')
+        self.assertEqual(self.trip.bike, 6270)
 
-        trip = Trip(data, 15305469)
-        self.assertEqual(trip.bike, 6270)
+    #def test_get_route(self):
+
+     #   self.trip.get_route()
 
 
