e51767df37497a930ad6a5d5e5b28bb871b174ca
James Bishop <jbishop@turing.ac.uk>
2022-06-14 10:11:08 +0100

    fix download ext

--- src/pfeffel/download.py
+++ src/pfeffel/download.py
@@ -1,18 +1,18 @@
-import os
-
-import requests
-from typing import List
-import time
-import shutil
-from tqdm.auto import tqdm
-import yaml
-from urllib.parse import urlparse
 import logging
+import os
+import shutil
+import time
 import zipfile
+from typing import List
+from urllib.parse import urlparse
+
 import pandas as pd
+import requests
+import yaml
+from tqdm.auto import tqdm
 
 try:
-    import xlrd
+    import xlrd  # noqa: F401
 except Exception as e:
     msg = (
         "Please install the package xlrd: `pip install --user xlrd`"
@@ -29,8 +29,6 @@ PROJECT_ROOT = os.path.abspath(
     os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
 )
 CONFIG_FPATH = os.path.join(PROJECT_ROOT, "config.yaml")
-
-
 
 
 def download_file(output_dir: str, url: str, sleep_seconds: float = 0) -> str:
@@ -60,9 +58,12 @@ def download_urls(
 ) -> List[str]:
     downloaded_fpaths = list()
     for url in tqdm(urls):
-        # ensure that we're doing a small sleep between calls so we don't overload API
+        # ensure that we're doing a small sleep between
+        #  calls so we don't overload API
         downloaded_fpaths.append(
-            download_file(output_dir=output_dir, url=url, sleep_seconds=sleep_seconds)
+            download_file(
+                output_dir=output_dir, url=url, sleep_seconds=sleep_seconds
+            )
         )
 
     return downloaded_fpaths
@@ -74,7 +75,9 @@ def extract_zips(zip_fpaths: List[str], csv_dir: str) -> None:
     for fpath in zip_fpaths:
         with zipfile.ZipFile(fpath, "r") as z:
             namelist = z.namelist()
-            has_been_extracted = any(name not in current_csvs for name in namelist)
+            has_been_extracted = any(
+                name not in current_csvs for name in namelist
+            )
             if has_been_extracted:
                 logging.info(f"Unzipping {fpath}")
                 z.extractall(csv_dir)
@@ -96,12 +99,15 @@ def xlsx_to_csv(csv_dir: str, xlsx_archive_dir: str) -> int:
         csvfile = xlsxfile.replace(".xlsx", ".csv")
         if not os.path.exists(csvfile):
             logging.info("Converting .xlsx to .csv.")
-            pd.read_excel(xlsxfile).to_csv(csvfile, date_format="%d/%m/%Y %H:%M:%S")
+            pd.read_excel(xlsxfile).to_csv(
+                csvfile, date_format="%d/%m/%Y %H:%M:%S"
+            )
         else:
             logging.debug("Already have {}".format(csvfile))
 
         shutil.move(
-            xlsxfile, os.path.join(xlsx_archive_dir, os.path.basename(xlsxfile))
+            xlsxfile,
+            os.path.join(xlsx_archive_dir, os.path.basename(xlsxfile)),
         )
 
     logging.info(f"moved {len(xlsx_files)} files")
@@ -126,8 +132,12 @@ if __name__ == "__main__":
     csv_urls = [url for url in urls if url.endswith(".csv")]
     zip_urls = [url for url in urls if url.endswith(".zip")]
 
-    csv_dir = os.path.join(data_root_dir, config["data"]["relative_paths"]["csvs_dir"])
-    zip_dir = os.path.join(data_root_dir, config["data"]["relative_paths"]["zips_dir"])
+    csv_dir = os.path.join(
+        data_root_dir, config["data"]["relative_paths"]["csvs_dir"]
+    )
+    zip_dir = os.path.join(
+        data_root_dir, config["data"]["relative_paths"]["zips_dir"]
+    )
     xlsx_dir = os.path.join(
         data_root_dir, config["data"]["relative_paths"]["xlsx_archive_dir"]
     )
