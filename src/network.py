9a78f6b5c416f781f2a9ac26f5b96c260b7b5f9b
Markus Hauru <mhauru@turing.ac.uk>
2022-06-16 12:36:14 +0100

    Move network plotting to network.py

--- src/network.py
+++ src/network.py
@@ -2,12 +2,36 @@ import json
 import pickle
 import re
 
+import cartopy.crs as ccrs
 import community
 import networkx as nx
+import numpy as np
 import pandas as pd
+from cartopy.io.img_tiles import OSM
+from matplotlib import pyplot as plt
 
 STATION_NAMES_FILE = "../data/station_names_20220615_1137.pickle"
 STATION_COORDS_FILE = "../data/stations_loc.json"
+
+TRIP_COUNT_THRESHOLD = 2e-5
+MAP_BOUNDARIES = (-0.223, 0.005, 51.46, 51.555)
+FONT_SIZE = 10
+EDGE_WIDTH_FACTOR = 1e-2
+MAX_NODE_SIZE = 250.0
+MIN_NODE_SIZE = 0.1
+MIN_EDGE_ALPHA = 0.1
+MAX_EDGE_ALPHA = 0.9
+EDGE_COLOUR = "#222222"
+LABEL_STATIONS = [
+    # "Belgrove Street",
+    # "Waterloo Station 3",
+    # "Hyde Park Corner",
+    # "Aquatic Centre",
+    # "Bethnal Green Road",
+    # "Natural History Museum",
+    # "Kennington Oval",
+    # "Mudchute DLR",
+]
 
 
 def get_station_name(id):
@@ -19,7 +43,7 @@ def get_station_name(id):
     return name
 
 
-def create_network_from_data(df, TRIP_COUNT_THRESHOLD):
+def create_network_from_data(df, trip_count_threshold):
     trip_counts = (
         (
             df[["start_station_id", "end_station_id", "bike_id"]]
@@ -33,7 +57,7 @@ def create_network_from_data(df, TRIP_COUNT_THRESHOLD):
     total_num_trips = trip_counts["trip_count"].sum()
 
     trip_counts = trip_counts[
-        trip_counts["trip_count"] >= TRIP_COUNT_THRESHOLD * total_num_trips
+        trip_counts["trip_count"] >= trip_count_threshold * total_num_trips
     ]
 
     graph = nx.from_pandas_edgelist(
@@ -86,3 +110,89 @@ def network_community_detection(graph, edge_weight):
 
 def visualise_network_map(network, node_info_df):
     pass
+
+
+def _scale_range(values, min_scaled, max_scaled):
+    values = np.array(values)
+    if min_scaled is not None:
+        max_value = np.max(values)
+        min_value = np.min(values)
+        mult_coeff = (max_scaled - min_scaled) / (max_value - min_value)
+        add_coeff = (max_value * min_scaled - min_value * max_scaled) / (
+            max_value - min_value
+        )
+        scaled = mult_coeff * values + add_coeff
+    else:
+        max_value = np.max(values)
+        scaled = max_scaled * values / max_value
+    return scaled
+
+
+def create_network_and_map(df):
+    community_graph = create_network_from_data(df, TRIP_COUNT_THRESHOLD)
+    nodes_info = get_node_info(community_graph)
+    visualisation_graph = community_graph.copy()
+    visualisation_graph.remove_edges_from(nx.selfloop_edges(community_graph))
+    community_df = network_community_detection(community_graph, "trip_count")
+    nodes_info = nodes_info.merge(community_df, on="id")
+    nodes_info = nodes_info.sort_values(by="size", ascending=False)
+    del community_df
+
+    weights = np.array(
+        [
+            visualisation_graph.edges[e]["trip_count"] * EDGE_WIDTH_FACTOR
+            for e in visualisation_graph.edges
+        ]
+    )
+    labels = {
+        id: name
+        for id, name in zip(nodes_info["id"], nodes_info["name"])
+        if name in LABEL_STATIONS
+    }
+
+    imagery = OSM(desired_tile_form="L")
+    fig, ax = plt.subplots(
+        1, 1, figsize=(10, 10), subplot_kw=dict(projection=imagery.crs)
+    )
+    ax.set_extent(MAP_BOUNDARIES)
+    ax.add_image(imagery, 14, cmap="gray")
+    xynps = ax.projection.transform_points(
+        ccrs.Geodetic(),
+        np.array([p[0] for p in nodes_info["pos"]]),
+        np.array([p[1] for p in nodes_info["pos"]]),
+    )
+    pos = {
+        k: (xynps[i, 0], xynps[i, 1]) for i, k in enumerate(nodes_info["id"])
+    }
+
+    sizes = _scale_range(nodes_info["size"], MIN_NODE_SIZE, MAX_NODE_SIZE)
+    edge_alpha = _scale_range(weights, MIN_EDGE_ALPHA, MAX_EDGE_ALPHA)
+
+    # Plots
+    nx.draw_networkx_nodes(
+        visualisation_graph,
+        pos=pos,
+        nodelist=nodes_info["id"],
+        node_color=nodes_info["partition"],
+        alpha=1.0,
+        node_size=sizes,
+        cmap="tab10_r",
+        ax=ax,
+    )
+    nx.draw_networkx_edges(
+        visualisation_graph,
+        pos=pos,
+        edge_color=EDGE_COLOUR,
+        width=weights,
+        alpha=edge_alpha,
+        arrows=True,
+        ax=ax,
+    )
+    nx.draw_networkx_labels(
+        visualisation_graph,
+        pos=pos,
+        labels=labels,
+        font_size=FONT_SIZE,
+        ax=ax,
+    )
+    plt.show()
